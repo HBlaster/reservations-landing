ba5e5fa549cf8abf071073dcd5ec9c9e0792dcd0
Alfredoc <alfredo10396.c@gmail.com>
2025-06-02 12:30:34 -0600

    dinamic form function added in the service for reservations

--- src/app/services/reservation-service.service.ts
+++ src/app/services/reservation-service.service.ts
@@ -3,16 +3,38 @@ import { ReservationDto } from '../models/reservation.dto';
 import { HttpClient } from '@angular/common/http';
 import { ReservationResponse } from '../models/reservation.response';
 import { environment } from '../../environments/environment';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReservationServiceService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,
+    private fb: FormBuilder
+  ) {
+  }
 
   createReservation(oReservation: ReservationDto) {
     const url = `${environment.apiUrl}reservation`;
     return this.http.post<ReservationResponse>(url, oReservation);
+  }
+
+  generateDynamicForm(frequency:string): FormGroup {
+    if (frequency === 'interval') {
+      return this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      officialId: ['', Validators.required],
+      reservationDay: ['', Validators.required],
+    });
+    } else {
+      return this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      officialId: ['', Validators.required],
+      reservationDay: ['', Validators.required],
+    });
+    }
   }
 
   getReservationConfig(){
@@ -20,11 +42,6 @@ export class ReservationServiceService {
     return this.http.get(url);
   }
 
-  // getAvailabilityByDate(date: string) {
-  //   const formattedDate = new Date(date).toISOString().split('T')[0];
-  //   const url = `${environment.apiUrl}config-reservation/availability/${formattedDate}`;
-  //   return this.http.get(url);
-  // }
   getAvailabilityByDate(date: string, type: 'daily' | 'interval' = 'daily') {
   const formattedDate = new Date(date).toISOString().split('T')[0];
   const url = `${environment.apiUrl}config-reservation/availability/${formattedDate}?type=${type}`;
